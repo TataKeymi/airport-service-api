bae358eac3ca7c4d4b045e379db4d59ec45b212a
unknown <tanakutova74@gmail.com>
2026-05-15 21:36:31 +0300

    Add API views and urls

---
+++ airport/urls.py
@@ -0,0 +1,26 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from airport.views import (CrewViewSet,
+                           AirportViewSet,
+                           RouteViewSet,
+                           AirplaneTypeViewSet,
+                           AirplaneViewSet, FlightViewSet, OrderViewSet, TicketViewSet)
+
+
+app_name = "airport"
+
+router = routers.DefaultRouter()
+
+router.register("crews", CrewViewSet)
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
