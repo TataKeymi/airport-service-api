9ef0be51ad213fef3ecb5fdd438d0ca179103db6
unknown <tanakutova74@gmail.com>
2026-05-16 22:51:08 +0300

    Add Django Debug Toolbar

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,4 +22,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/airport/", include("airport.urls", namespace="airport")),
-]
+] + debug_toolbar_urls()
